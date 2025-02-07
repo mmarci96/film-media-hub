9daf81be961e68cc2c7bfa9f89455e29e038ef41
mmarci96 <sarosdimarci@gmail.com>
2025-02-07 21:50:56 +0100

    server initial state

---
+++ filmhub-server/src/db/userModel.js
@@ -0,0 +1,12 @@
+import mongoose, { Schema } from 'mongoose';
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date },
+    user_details: { type: Schema.Types.ObjectId, ref: 'UserDetails' },
+});
+
+const UserModel = mongoose.model('User', userSchema);
+export default UserModel;
---
+++ filmhub-server/src/index.js
@@ -0,0 +1,54 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+import authRoutes from './routes/authRoutes';
+import errorHandler from './middleware/errorHandler';
+
+dotenv.config();
+
+const app = express();
+const { MONGO_URI, PORT = 8080 } = process.env;
+
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:80'];
+
+const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+};
+
+app.use(cors(options));
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/media', mediaRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/follows', followRoutes);
+app.use('/api/likes', likeRoutes);
+app.use('/api/notifications', notificationRoutes);
+app.get('/api/hello', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.use(errorHandler);
+
+const main = async () => {
+    const url = MONGO_URI;
+    if (!url) {
+        console.error('Missing MONGO_URL environment variable');
+        process.exit(1);
+    }
+    await mongoose.connect(url);
+
+    app.listen(PORT as number, '0.0.0.0', () => {
+        console.log('App is listening on ', PORT);
+    });
+};
+
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+}
