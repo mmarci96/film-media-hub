96a49830805ffe36cd368e1c2eb14a1dadbf91da
mmarci96 <sarosdimarci@gmail.com>
2025-02-08 05:05:02 +0100

    lil fix

--- client/src/components/media-card.tsx
+++ client/src/components/media-card.tsx
@@ -11,12 +11,12 @@ const MediaCard: React.FC<MediaCardProps> = ({ media, mediaType }) => {
     return (
         <MediaPopup media={media} mediaType={mediaType}
             children={
-                <Card key={media.id} className="rounded-lg my-4 mx-1 max-w-[320px] h-[420px] text-elipsis overflow-hidden shadow-md ring-2 hover:scale-105 cursor-pointer">
+                <Card key={media.id} className="rounded-lg my-4 mx-1 max-w-[320px] h-[440px] text-elipsis overflow-hidden shadow-md ring-2 hover:scale-105 cursor-pointer">
                     <CardBody className="p-0 text-elipsis overflow-hidden">
                         <img
                             src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
                             alt={media.title || "Movie Poster"}
-                            className="w-[320px] max-h-[380px] rounded-xl pb-0 p-1 object-cover"
+                            className="w-[320px] max-h-[400px] rounded-xl pb-0 p-1 object-cover"
                         />
                         <div className="flex justify-center p-2">
                             <h1 className="text-lg font-bold"> {media.name || media.title}
