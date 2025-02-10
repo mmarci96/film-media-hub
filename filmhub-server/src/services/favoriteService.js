3ab21d7c980bdc495af0c65d0e93741e57b387d8
mmarci96 <sarosdimarci@gmail.com>
2025-02-10 22:25:04 +0100

    service layer for the favorites

---
+++ filmhub-server/src/services/favoriteService.js
@@ -0,0 +1,56 @@
+import UserFavoriteModel from "../db/user.favorite.model";
+import createBadRequestError from "../errors/badRequestError";
+
+export const saveFavoriteMedia = async ({
+    userId, mediaId, mediaType
+}) => {
+    if (!userId || !mediaId || !mediaType) {
+        return createBadRequestError(400, "missing data to save favorite");
+    }
+
+    const existing = await UserFavoriteModel.findOne({ userId, mediaId, mediaType });
+    if (existing) {
+        return createBadRequestError(403, ", save the same media twice")
+    }
+
+    const saveFavorite = new UserFavoriteModel({
+        userId, mediaId, mediaType
+    })
+    const favorite = await UserFavoriteModel.save();
+    if (!favorite) {
+        return createBadRequestError(400, "favorites")
+    }
+
+    return favorite;
+}
+
+export const deleteFavoriteMedia = async ({
+    userId, favoriteId
+}) => {
+    if (!userId || !mediaId || !mediaType) {
+        return createBadRequestError(400, "missing data to save favorite");
+    }
+
+    const deleted = await UserFavoriteModel.findOneAndDelete({
+        _id: favoriteId, userId
+    })
+    if (!deleted) {
+        return createBadRequestError(404, "favorite")
+    }
+
+    return deleted;
+}
+
+export const getFavoritesByUserId = async (userId) => {
+    if (!userId) {
+        return createBadRequestError(400, "no id provided")
+    }
+
+    const favoriteList = await UserFavoriteModel
+        .find({ userId })
+        .sort({ createdAt: -1 })
+    if (!favoriteList) {
+        return createBadRequestError(404, 'no favorites')
+    }
+    return favoriteList;
+}
