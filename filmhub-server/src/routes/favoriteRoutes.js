1b2a98c556d6a6660434794b2e6ef1ba43bcf804
mmarci96 <sarosdimarci@gmail.com>
2025-02-10 22:38:16 +0100

    routes to save delete and get

---
+++ filmhub-server/src/routes/favoriteRoutes.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { authenticateToken } from '../middleware/authToken';
+import { getFavoritesByUserId } from '../services/userService';
+
+const router = express.Router();
+
+router.get('/', authenticateToken, async (req, res, next) => {
+    try {
+        const { userId } = req;
+        const favorites = await getFavoritesByUserId(userId);
+
+        res.status(200).send({ data: favorites });
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.post('/', authenticateToken, async (req, res, next) => {
+    try {
+        const { userId } = req;
+        const { mediaId, mediaType } = req.body;
+
+        const result = await saveFavoriteMedia({
+            userId,
+            mediaId,
+            mediaType
+        });
+
+        res.status(201).send({ data: result });
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.delete('/:favoriteId', authenticateToken, async (req, res, next) => {
+    try {
+        const { userId } = req;
+        const { favoriteId } = req.params;
+
+        const result = await deleteFavoriteMedia({
+            userId,
+            favoriteId
+        });
+
+        res.status(200).send({
+            data: result,
+            message: 'Favorite successfully deleted'
+        });
+    } catch (err) {
+        next(err);
+    }
+});
+
+export default router;
+
