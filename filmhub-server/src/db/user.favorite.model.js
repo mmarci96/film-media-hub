e5883f2fcfb183d528373fc831628eae90d014bc
mmarci96 <sarosdimarci@gmail.com>
2025-02-10 23:24:02 +0100

    slight naming issue

--- filmhub-server/src/db/user.favorite.model.js
+++ filmhub-server/src/db/user.favorite.model.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from "mongoose";
 const userFavoriteSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     mediaType: { type: String, required: true },
-    favoriteId: { type: String, required: true },
+    mediaId: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date },
 })
