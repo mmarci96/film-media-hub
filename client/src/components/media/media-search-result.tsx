1b070b22aeaf4fb4a7de94447ae53ec93dc41a83
mmarci96 <sarosdimarci@gmail.com>
2025-02-11 10:25:18 +0100

    search results are looking like something, cannot close and click on results yet

--- client/src/components/media/media-search-result.tsx
+++ client/src/components/media/media-search-result.tsx
@@ -1,15 +1,37 @@
 import { MediaItem } from "@/types"
+import { Card, CardHeader, CardBody, Button } from "@heroui/react"
+import { Link } from "react-router-dom";
 
 
 const MediaSearchResult = ({ results }: { results: MediaItem[] }) => {
     return (
-        <div className="absolute z-30 bg-background/80 flex flex-col items-center w-screen h-screen top-[50%] ">
+        <Card
+            isBlurred
+            className="absolute z-30 pt-4 bg-background/20 bg-foreground flex flex-col items-center w-[82vw] top-16 h-[92vh] m-4 rounded-xl ring-1 left-[50%] translate-x-[-50%] overflow-scroll">
             {results?.map(media => (
-                <div key={media.id} className="w-full">
-                    <p>{media?.name || media.title}</p>
+                <div key={media.id} className="w-full mx-auto my-1">
+                    <Card
+                        isBlurred
+                        className="w-full min-w-[80vw] max-w-[800px] m-1 p-4 mx-auto bg-background/60"
+                    >
+                        <CardHeader className="flex justify-between gap-3">
+                            <h3 className="text-lg">{media?.name || media.title}</h3>
+                            <Link to={`/${media.mediaType}/${media.id}`}>
+                                <Button variant="faded" className="bg-opacity-80">
+                                    More info
+                                </Button>
+                            </Link>
+                        </CardHeader>
+                        <CardBody>
+                            <p className="text-sm truncate">
+                                {media?.overview}
+                            </p>
+                        </CardBody>
+
+                    </Card>
                 </div>
             ))}
-        </div>
+        </Card>
     )
 }
 
--- client/src/components/nav/navbar.tsx
+++ client/src/components/nav/navbar.tsx
@@ -12,12 +12,9 @@ import {
 } from "@heroui/navbar";
 import { link as linkStyles } from "@heroui/theme";
 import clsx from "clsx";
-
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/nav/theme-switch";
-import {
-    SearchIcon,
-} from "@/components/nav/icons";
+import { SearchIcon } from "@/components/nav/icons";
 import DropDownComponent from "./drop-down";
 import { useTmdbDetails } from "@/hooks/use-tmdb-details";
 import { useEffect, useState } from "react";
@@ -66,83 +63,85 @@ export const Navbar = () => {
     );
 
     return (
-        <HeroUINavbar maxWidth="xl" position="sticky" className="z-20">
+        <>
             {searchResults && <MediaSearchResult results={searchResults.results} />}
-            <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-                <NavbarBrand className="gap-3 max-w-fit">
-                    <Link
-                        className="flex justify-start items-center gap-1"
-                        color="foreground"
-                        href="/"
-                    >
-                        <img src="./logo.png"
-                            className={"hover:scale-105 w-8 h-8 border-4 border-[#181a2f]" +
-                                "object-fit rounded-full ease-in duration-300 animation-spin"} />
-                        <p className="font-bold text-lg py-1 px-2">FilmHub</p>
-                    </Link>
-                </NavbarBrand>
-                <div className="hidden md:flex gap-4 justify-start ml-4 ring-1 ring-[#26233A] rounded-xl px-4 p-2">
-                    {siteConfig.navItems.map((item) => (
-                        <NavbarItem key={item.href}
-                            className={"hover:scale-110 hover:ring-1 bg-secondgrey ring-[#2C647C]  rounded-lg hover:px-2  hover:tracking-wider ease-in duration-200 "}
+            <HeroUINavbar maxWidth="xl" position="sticky" className="z-20">
+                <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+                    <NavbarBrand className="gap-3 max-w-fit">
+                        <Link
+                            className="flex justify-start items-center gap-1"
+                            color="foreground"
+                            href="/"
                         >
-                            <Link
-                                className={clsx(
-                                    linkStyles({ color: "foreground" }),
-                                    "data-[active=true]:text-primary text-md  ease-in duration-200 data-[active=true]:font-medium",
-                                )}
-                                color="foreground"
-                                href={item.href}
+                            <img src="./logo.png"
+                                className={"hover:scale-105 w-8 h-8 border-4 border-[#181a2f]" +
+                                    "object-fit rounded-full ease-in duration-300 animation-spin"} />
+                            <p className="font-bold text-lg py-1 px-2">FilmHub</p>
+                        </Link>
+                    </NavbarBrand>
+                    <div className="hidden md:flex gap-4 justify-start ml-4 ring-1 ring-[#26233A] rounded-xl px-4 p-2">
+                        {siteConfig.navItems.map((item) => (
+                            <NavbarItem key={item.href}
+                                className={"hover:scale-110 hover:ring-1 bg-secondgrey ring-[#2C647C]  rounded-lg hover:px-2  hover:tracking-wider ease-in duration-200 "}
                             >
-                                {item.label}
-                            </Link>
-                        </NavbarItem>
-                    ))}
-                </div>
-            </NavbarContent>
+                                <Link
+                                    className={clsx(
+                                        linkStyles({ color: "foreground" }),
+                                        "data-[active=true]:text-primary text-md  ease-in duration-200 data-[active=true]:font-medium",
+                                    )}
+                                    color="foreground"
+                                    href={item.href}
+                                >
+                                    {item.label}
+                                </Link>
+                            </NavbarItem>
+                        ))}
+                    </div>
+                </NavbarContent>
 
-            <NavbarContent
-                className="hidden sm:flex basis-1/5 sm:basis-full"
-                justify="end"
-            >
-                <NavbarItem className="hidden sm:flex gap-2">
-                    <NavTipTool
-                        tipContent={"Click to see options"}
-                        children={<DropDownComponent />}
-                    />
+                <NavbarContent
+                    className="hidden sm:flex basis-1/5 sm:basis-full"
+                    justify="end"
+                >
+                    <NavbarItem className="hidden sm:flex gap-2">
+                        <NavTipTool
+                            tipContent={"Click to see options"}
+                            children={<DropDownComponent />}
+                        />
+                        <ThemeSwitch />
+                    </NavbarItem>
+                    <NavbarItem className="hidden md:flex">{searchInput}</NavbarItem>
+                </NavbarContent>
+
+                <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+                    <DropDownComponent />
                     <ThemeSwitch />
-                </NavbarItem>
-                <NavbarItem className="hidden md:flex">{searchInput}</NavbarItem>
-            </NavbarContent>
+                    <NavbarMenuToggle />
+                </NavbarContent>
 
-            <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-                <DropDownComponent />
-                <ThemeSwitch />
-                <NavbarMenuToggle />
-            </NavbarContent>
-
-            <NavbarMenu>
-                {searchInput}
-                <div className="mx-4 mt-2 flex flex-col gap-2">
-                    {siteConfig.navMenuItems.map((item, index) => (
-                        <NavbarMenuItem key={`${item}-${index}`}>
-                            <Link
-                                color={
-                                    index === 2
-                                        ? "primary"
-                                        : index === siteConfig.navMenuItems.length - 1
-                                            ? "danger"
-                                            : "foreground"
-                                }
-                                href="#"
-                                size="lg"
-                            >
-                                {item.label}
-                            </Link>
-                        </NavbarMenuItem>
-                    ))}
-                </div>
-            </NavbarMenu>
-        </HeroUINavbar>
+                <NavbarMenu>
+                    {searchInput}
+                    <div className="mx-4 mt-2 flex flex-col gap-2">
+                        {siteConfig.navMenuItems.map((item, index) => (
+                            <NavbarMenuItem key={`${item}-${index}`}>
+                                <Link
+                                    color={
+                                        index === 2
+                                            ? "primary"
+                                            : index === siteConfig.navMenuItems.length - 1
+                                                ? "danger"
+                                                : "foreground"
+                                    }
+                                    href="#"
+                                    size="lg"
+                                >
+                                    {item.label}
+                                </Link>
+                            </NavbarMenuItem>
+                        ))}
+                    </div>
+                </NavbarMenu>
+            </HeroUINavbar>
+        </>
     );
 };
