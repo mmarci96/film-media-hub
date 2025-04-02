5c937b7aa31f71c649a9e808e1655ce4e444ea8f
mmarci96 <sarosdimarci@gmail.com>
2025-04-02 12:18:04 +0200

    for debugging the media is logged when on movies endpoint

--- client/src/pages/media.tsx
+++ client/src/pages/media.tsx
@@ -7,30 +7,29 @@ import { useTmdbDetails } from "@/hooks/use-tmdb-details";
 import MediaFullPage from "@/components/media/media-full-page";
 
 const MediaPage = () => {
-    const [mediaData, setMediaData] = useState<MediaDetails | null>(null)
+    const [mediaData, setMediaData] = useState<MediaDetails | null>(null);
     const { fetchMediaDetails } = useTmdbDetails();
-    const { fetchAnimeDetails } = useAnimeDetails()
-    const { id, mediaType = 'tv' as MediaType } = useParams()
+    const { fetchAnimeDetails } = useAnimeDetails();
+    const { id, mediaType = "tv" as MediaType } = useParams();
     const handleMedia = async (id: string, mediaType: MediaType) => {
-        if (mediaType === 'anime') {
-            const data = await fetchAnimeDetails(id)
-            data && setMediaData(data)
+        if (mediaType === "anime") {
+            const data = await fetchAnimeDetails(id);
+            data && setMediaData(data);
         } else {
-            const data = await fetchMediaDetails(parseInt(id), mediaType)
-            data && setMediaData(data)
+            const data = await fetchMediaDetails(parseInt(id), mediaType);
+            data && setMediaData(data);
         }
-    }
+    };
 
     useEffect(() => {
-        id && handleMedia(id, mediaType as MediaType)
-    }, [])
+        id && handleMedia(id, mediaType as MediaType);
+    }, []);
     return (
         <DefaultLayout>
             <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-
                 {mediaData && <MediaFullPage mediaDetails={mediaData} />}
             </section>
         </DefaultLayout>
     );
-}
+};
 export default MediaPage;
--- client/src/pages/movies.tsx
+++ client/src/pages/movies.tsx
@@ -1,18 +1,39 @@
 import { title } from "@/components/primitives";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DefaultLayout from "@/layouts/default";
 import MediaList from "@/components/media/media-list";
 import { useTmdb } from "@/hooks/use-tmdb";
+import { useAuth } from "@/hooks/use-auth";
 import { ListType } from "@/types";
 
 export default function MoviesPage() {
     const [page, setPage] = useState(1);
-    const [listType, setListType] = useState<ListType>("popular")
-    const mediaType = 'movie'
+    const [listType, setListType] = useState<ListType>("popular");
+    const mediaType = "movie";
     const { mediaList, error, loading } = useTmdb(mediaType, page, listType);
     const onLoadMore = (page: number) => {
         setPage(page);
-    }
+    };
+
+    const { token } = useAuth();
+    const fetchMedia = async (token: string) => {
+        const res = await fetch("/api/v1/media", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!res.ok) {
+            console.error(res);
+        }
+        const { media } = await res.json();
+        console.log(media);
+    };
+
+    useEffect(() => {
+        token && fetchMedia(token);
+    }, [token]);
 
     return (
         <DefaultLayout>
@@ -32,4 +53,3 @@ export default function MoviesPage() {
         </DefaultLayout>
     );
 }
-
