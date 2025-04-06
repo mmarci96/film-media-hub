cde3a64acfb74a56399e1e5ee6a7e2e2fb8133dd
mmarci96 <sarosdimarci@gmail.com>
2025-04-06 09:03:57 +0200

    anime displaying

--- client/src/hooks/use-anime-details.ts
+++ client/src/hooks/use-anime-details.ts
@@ -2,18 +2,19 @@ import { MediaDetails } from "@/types";
 import { useState } from "react";
 import { AnimeItem } from "@/types";
 
-
 export const useAnimeDetails = () => {
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState<any>(null)
-    const fetchAnimeDetails = async (animeId: string): Promise<MediaDetails | void> => {
-        setLoading(true)
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<any>(null);
+    const fetchAnimeDetails = async (
+        animeId: string,
+    ): Promise<MediaDetails | void> => {
+        setLoading(true);
         try {
-            const res = await fetch(`https://api.jikan.moe/v4/anime/${animeId}/full`);
-            const { data } = await res.json()
+            const res = await fetch(`/api/v1/anime/${animeId}`);
+            const data = await res.json();
             if (!res.ok) {
-                setLoading(false)
-                setError("No response from server...")
+                setLoading(false);
+                setError("No response from server...");
             }
             const details: AnimeItem = data;
             const date = details?.aired?.from
@@ -22,7 +23,7 @@ export const useAnimeDetails = () => {
 
             const mediaDetails: MediaDetails = {
                 id: details.mal_id,
-                mediaType: 'anime',
+                mediaType: "anime",
                 title: details.title,
                 original_title: details.title_japanese,
                 overview: details.synopsis,
@@ -34,16 +35,15 @@ export const useAnimeDetails = () => {
                 poster_path: details?.images?.jpg?.image_url,
                 popularity: details.popularity,
                 video: true,
-            }
+            };
 
-            setLoading(false)
+            setLoading(false);
             return mediaDetails;
         } catch (err) {
-            setLoading(false)
-            setError(err)
+            setLoading(false);
+            setError(err);
         }
-    }
+    };
 
     return { fetchAnimeDetails, error, loading };
-}
-
+};
